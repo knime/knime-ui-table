cc19a0aa610739062bd9f8daa817acd02a637328
Daniel Bogenrieder <daniel.bogenrieder@knime.com>
2023-01-23 09:54:47 +0100

    wip

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,36 +1,33 @@
-const {
-  mergeConfig
-} = require('vite');
-const path = require('node:path');
-const {
-  fileURLToPath,
-  URL
-} = require('node:url');
-const svgLoader = require('vite-svg-loader');
+const { mergeConfig } = require("vite");
+const path = require("node:path");
+const { fileURLToPath, URL } = require("node:url");
+const svgLoader = require("vite-svg-loader");
 module.exports = {
-  "stories": ["**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
-  "framework": {
+  stories: ["**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
     name: "@storybook/vue3-vite",
-    options: {}
+    options: {},
   },
-  async viteFinal(config, {
-    configType
-  }) {
+  async viteFinal(config, { configType }) {
     return mergeConfig(config, {
       plugins: [svgLoader()],
       resolve: {
         alias: {
-          '@': path.resolve(__dirname, '../src'),
-          'webapps-common': path.resolve(__dirname, '../webapps-common')
-        }
-      }
+          "@": path.resolve(__dirname, "../src"),
+          "webapps-common": path.resolve(__dirname, "../webapps-common"),
+        },
+      },
     });
   },
-  "features": {
-    "storyStoreV7": true
+  features: {
+    storyStoreV7: true,
   },
   docs: {
-    autodocs: true
-  }
+    autodocs: true,
+  },
 };
