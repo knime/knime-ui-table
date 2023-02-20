d7f365e145edaa48cfd83dfc880edd1d766408fb
Christian Albrecht <christian.albrecht@knime.com>
2023-02-20 11:37:46 +0100

    Remove jest reference, ignore config import violations

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,16 +15,25 @@ module.exports = {
             }
         }
     },
+    rules: {
+        'import/extensions': [
+            'error',
+            { vue: 'always', json: 'always', mjs: 'always', svg: 'always', config: 'ignorePackages' }
+        ]
+    },
     overrides: [
         {
-            extends: ['@knime/eslint-config/jest'],
             files: ['src/**/__tests__/**', 'test-setup/**'],
             rules: {
                 'no-magic-numbers': 'off',
+                'no-undefined': 'off',
                 'import/extensions': [
                     'error',
                     { vue: 'always', json: 'always', mjs: 'always', svg: 'always', config: 'ignorePackages' }
                 ]
+            },
+            env: {
+                node: true
             }
         }
     ]
