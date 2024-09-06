254deaa3a2a15632d2c2839c32123824f20cc46c
Paul Bärnreuther <paul.baernreuther@tngtech.com>
2024-09-06 16:45:17 +0200

    UIEXT-2133: Move TopControls before Table
    Before, the caption inside the table lead to wrong page-break behaviors
    for headers
    
    UIEXT-2133 (Fix Table issues in reporting)

--- src/components/control/BaseControls.vue
+++ src/components/control/BaseControls.vue
@@ -41,77 +41,73 @@ export default {
 </script>
 
 <template>
-  <caption>
-    <div class="row">
-      <PageControls
-        :total-items="pageConfig.tableSize"
-        :show-page-controls="pageConfig.showPageControls"
-        :current-items="pageConfig.currentSize"
-        :page-size="pageConfig.pageSize"
-        :current-page="pageConfig.currentPage"
-        :column-count="pageConfig.columnCount"
-        :show-table-size="pageConfig.showTableSize"
-        @next-page="onNextPage"
-        @prev-page="onPrevPage"
-      />
-      <div class="controls right-controls">
-        <div v-if="hasCarousel" ref="carousel-wrapper" class="carousel-wrapper">
-          <Carousel>
-            <div class="wrapper">
-              <slot name="carousel" />
-            </div>
-          </Carousel>
-        </div>
-        <div class="wrapper">
-          <slot name="rightmost-control" />
-        </div>
+  <div class="row">
+    <PageControls
+      :total-items="pageConfig.tableSize"
+      :show-page-controls="pageConfig.showPageControls"
+      :current-items="pageConfig.currentSize"
+      :page-size="pageConfig.pageSize"
+      :current-page="pageConfig.currentPage"
+      :column-count="pageConfig.columnCount"
+      :show-table-size="pageConfig.showTableSize"
+      @next-page="onNextPage"
+      @prev-page="onPrevPage"
+    />
+    <div class="controls right-controls">
+      <div v-if="hasCarousel" ref="carousel-wrapper" class="carousel-wrapper">
+        <Carousel>
+          <div class="wrapper">
+            <slot name="carousel" />
+          </div>
+        </Carousel>
+      </div>
+      <div class="wrapper">
+        <slot name="rightmost-control" />
       </div>
     </div>
-  </caption>
+  </div>
 </template>
 
 <style lang="postcss" scoped>
-caption {
-  & .row {
-    display: flex;
-    justify-content: space-between;
+.row {
+  display: flex;
+  justify-content: space-between;
+  padding: 0;
+  margin: 0;
+  height: 40px;
+
+  & .controls {
     padding: 0;
-    margin: 0;
-    height: 40px;
+    font-weight: 500;
+    color: var(--knime-dove-gray);
 
-    & .controls {
-      padding: 0;
-      font-weight: 500;
-      color: var(--knime-dove-gray);
+    &.right-controls {
+      min-width: 0;
+      display: flex;
+      justify-content: flex-end;
 
-      &.right-controls {
+      & .wrapper {
+        flex-flow: row nowrap;
+        display: inline-flex;
+        align-items: center;
+        text-align: left;
+
+        & span {
+          line-height: 50px;
+        }
+      }
+
+      & .carousel-wrapper {
         min-width: 0;
-        display: flex;
-        justify-content: flex-end;
 
         & .wrapper {
-          flex-flow: row nowrap;
-          display: inline-flex;
-          align-items: center;
-          text-align: left;
-
-          & span {
-            line-height: 50px;
-          }
+          padding-left: 0;
+          padding-right: 0;
         }
 
-        & .carousel-wrapper {
-          min-width: 0;
-
-          & .wrapper {
-            padding-left: 0;
-            padding-right: 0;
-          }
-
-          & :deep(.shadow-wrapper) {
-            margin-right: 0;
-            margin-left: 0;
-          }
+        & :deep(.shadow-wrapper) {
+          margin-right: 0;
+          margin-left: 0;
         }
       }
     }
